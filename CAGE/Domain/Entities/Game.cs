f9821c05285471818a3786371fa63127a7e341b1
James Roberts <James@Robertscom.com>
2021-12-30 17:15:21 +1100

    Add Game
    CAGE-1

---
+++ CAGE/Domain/Entities/Game.cs
@@ -0,0 +1,19 @@
+﻿namespace CAGE.Domain.Entities
+{
+
+    public class Game
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public EntityID ID { get; } = new();
+
+        public Turn CurrentTurn { get; } = new() { TurnNumber = 1 };
+
+        public ICollection<Turn> PreviousTurns { get; } = new List<Turn>();
+
+        #endregion Properties
+
+    }
+
+}
