dd63e53962b3c5c80c982c5979f876e336a2d5fe
James Roberts <James@Robertscom.com>
2021-12-31 18:08:21 +1100

    Add Dtos
    CAGE-2

---
+++ CAGE/Application/Dtos/PlayerDto.cs
@@ -0,0 +1,29 @@
+﻿using CAGE.Domain.Entities;
+
+namespace CAGE.Application.Dtos
+{
+
+    public class PlayerDto
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        public PlayerDto(Player player)
+        {
+            this.Name = player.Name;
+            this.PlayerID = player.ID;
+        }
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        public string Name { get; set; }
+
+        public EntityID PlayerID { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
