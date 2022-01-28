5d3f1af013f09cc2f7c279fec3b1951c4ed5d285
James Roberts <James@Robertscom.com>
2022-01-28 23:21:07 +1100

    Add the IPhaseEngineStrategy interface
    CAGE-3

---
+++ Cage.PhaseEngine/IPhaseEngineStrategy.cs
@@ -0,0 +1,17 @@
+﻿namespace Cage.PhaseEngine
+{
+
+    public interface IPhaseEngineStrategy<TPhase, TPlayer> where TPhase : IPhase
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        (TPhase Phase, TPlayer? Player, int? RoundNumber)[] GetActivePhases();
+
+        bool TryEndPhase(TPhase phase, TPlayer? player);
+
+        #endregion Methods
+
+    }
+
+}
