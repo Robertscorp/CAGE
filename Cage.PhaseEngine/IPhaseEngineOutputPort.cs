9f6d2803522b5641b74e8397feb83daaf6f447a2
James Roberts <James@Robertscom.com>
2022-01-28 23:22:34 +1100

    Add the IPhaseEngineOutputPort interface
    CAGE-3

---
+++ Cage.PhaseEngine/IPhaseEngineOutputPort.cs
@@ -0,0 +1,27 @@
+﻿namespace Cage.PhaseEngine
+{
+
+    public interface IPhaseEngineOutputPort<TPhase, TPlayer> where TPhase : IPhase
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        Task EngineAlreadyRunningAsync(CancellationToken cancellationToken);
+
+        Task EnginePausedAsync(CancellationToken cancellationToken);
+
+        Task EngineResumedAsync(CancellationToken cancellationToken);
+
+        Task EngineStartedAsync(CancellationToken cancellationToken);
+
+        Task EngineStoppedAsync(CancellationToken cancellationToken);
+
+        Task NonPlayerPhaseAsync(TPhase phase, int? round, CancellationToken cancellationToken);
+
+        Task PlayerPhaseAsync(TPhase phase, TPlayer player, int? round, CancellationToken cancellationToken);
+
+        #endregion Methods
+
+    }
+
+}
