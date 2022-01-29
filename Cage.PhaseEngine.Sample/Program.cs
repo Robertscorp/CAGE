b102c18ae7361631f9a6d1559853a38830fc3e79
James Roberts <James@Robertscom.com>
2022-01-29 21:27:12 +1100

    Add the ability to pick the RandomPlayer PhaseEngineStrategy
    CAGE-6

--- Cage.PhaseEngine.Sample/Program.cs
+++ Cage.PhaseEngine.Sample/Program.cs
@@ -6,14 +6,15 @@ using Cage.PhaseEngine.Strategies;
 var _Phases = new[] { Phase.Phase1, Phase.Phase2, Phase.Phase3 };
 var _Players = new[] { new Player { Name = "Player 1" }, new Player { Name = "Player 2" }, new Player { Name = "Player 3" } };
 var _Presenter = new Presenter();
-var _Strategy = default(IPhaseEngineStrategy<Phase, Player>);// 
+var _Strategy = default(IPhaseEngineStrategy<Phase, Player>);
 
 Console.WriteLine("All Phase Engine Strategies will go through each Phase sequentially.");
 Console.WriteLine();
 Console.WriteLine("Pick a Phase Engine Strategy:");
 Console.WriteLine(" [1] Ranked - Each Player is given their turn sequentially, in Player order.");
 Console.WriteLine(" [2] Round Robin - Each Player is given their turn sequentially, starting in Player order. Each Round the first Player will move to the next Player.");
-Console.WriteLine(" [3] Simultaneous - All Players in a Phase take their turn at the same time.");
+Console.WriteLine(" [3] Random - Each Player is given their turn randomly.");
+Console.WriteLine(" [4] Simultaneous - All Players in a Phase take their turn at the same time.");
 Console.WriteLine(" [Other] Exit.");
 
 if (!int.TryParse(Console.ReadLine(), out var _Option))
@@ -23,7 +24,8 @@ switch (_Option)
 {
     case 1: _Strategy = new RankedPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
     case 2: _Strategy = new RoundRobinPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
-    case 3: _Strategy = new SimultaneousPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
+    case 3: _Strategy = new RandomPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
+    case 4: _Strategy = new SimultaneousPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
 }
 
 if (_Strategy == null)
