7d7e79b1f05548a548d11c5be18001f1aa929387
James Roberts <James@Robertscom.com>
2022-01-29 12:37:09 +1100

    Dispose the Interactor
    CAGE-3

--- Cage.PhaseEngine.Sample/Program.cs
+++ Cage.PhaseEngine.Sample/Program.cs
@@ -7,22 +7,23 @@ var _Phases = new[] { Phase.Phase1, Phase.Phase2, Phase.Phase3 };
 var _Players = new[] { new Player { Name = "Player 1" }, new Player { Name = "Player 2" }, new Player { Name = "Player 3" } };
 var _Presenter = new Presenter();
 var _Strategy = new SequentialRankedPhaseEngineStrategy<Phase, Player>(_Phases, _Players);
-var _Interactor = (IPhaseEngineInputPort<Phase, Player>)new PhaseEngineInteractor<Phase, Player>(_Presenter);
 
-var _StartAsync = _Interactor.StartAsync(_Strategy, default);
+using var _Interactor = new PhaseEngineInteractor<Phase, Player>(_Presenter);
+var _InputPort = (IPhaseEngineInputPort<Phase, Player>)_Interactor;
+var _StartAsync = _InputPort.StartAsync(_Strategy, default);
 
 for (var _I = 0; _I < 5; _I++)
 {
     await Task.Delay(1500);
-    await _Interactor.PauseAsync(default);
+    await _InputPort.PauseAsync(default);
     await Task.Delay(1500);
-    await _Interactor.ResumeAsync(default);
+    await _InputPort.ResumeAsync(default);
 }
 
 _ = Task.Delay(10500).ContinueWith(task =>
 {
     Console.WriteLine("Telling the Phase Engine to stop...");
-    return _Interactor.StopAsync(default);
+    return _InputPort.StopAsync(default);
 });
 
 await _StartAsync;
