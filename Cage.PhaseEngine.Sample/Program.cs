faaed447e9cf6f595f3d3e7eb6b45582f2128407
James Roberts <James@Robertscom.com>
2022-01-30 15:58:14 +1100

    Add the ability to pick the PlayerInitiative PhaseEngineStrategy
    CAGE-9

--- Cage.PhaseEngine.Sample/Program.cs
+++ Cage.PhaseEngine.Sample/Program.cs
@@ -4,7 +4,7 @@ using Cage.PhaseEngine.Sample;
 using Cage.PhaseEngine.Strategies;
 
 var _Phases = new[] { Phase.Phase1, Phase.Phase2, Phase.Phase3 };
-var _Players = new[] { new Player { Name = "Player 1" }, new Player { Name = "Player 2" }, new Player { Name = "Player 3" } };
+var _Players = new[] { new Player(1, "Player 1"), new Player(5, "Player 2"), new Player(10, "Player 3") };
 var _Presenter = new Presenter();
 var _Strategy = default(IPhaseEngineStrategy<Phase, Player>);
 
@@ -14,7 +14,8 @@ Console.WriteLine("Pick a Phase Engine Strategy:");
 Console.WriteLine(" [1] Ranked - Each Player is given their turn sequentially, in Player order.");
 Console.WriteLine(" [2] Round Robin - Each Player is given their turn sequentially, starting in Player order. Each Round the first Player will move to the next Player.");
 Console.WriteLine(" [3] Random - Each Player is given their turn sequentially, in a random order.");
-Console.WriteLine(" [4] Simultaneous - All Players in a Phase take their turn at the same time.");
+Console.WriteLine(" [4] Initiative - Each Player is given their turn sequentially, in Initiative order (P3 = 10, P2 = 5, P1 = 1).");
+Console.WriteLine(" [5] Simultaneous - All Players in a Phase take their turn at the same time.");
 Console.WriteLine(" [Other] Exit.");
 
 if (!int.TryParse(Console.ReadLine(), out var _Option))
@@ -25,7 +26,8 @@ switch (_Option)
     case 1: _Strategy = new RankedPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
     case 2: _Strategy = new RoundRobinPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
     case 3: _Strategy = new RandomPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
-    case 4: _Strategy = new SimultaneousPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
+    case 4: _Strategy = new PlayerInitiativePhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
+    case 5: _Strategy = new SimultaneousPlayerPhaseEngineStrategy<Phase, Player>(_Phases, _Players); break;
 }
 
 if (_Strategy == null)
