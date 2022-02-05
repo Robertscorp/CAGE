88efd984d637f6d732516a5097bd3d76e277082b
James Roberts <James@Robertscom.com>
2022-02-05 20:34:00 +1100

    Add the Tick Engine InputPort
    CAGE-7

---
+++ Cage.TickEngine/ITickEngineInputPort.cs
@@ -0,0 +1,21 @@
+﻿namespace Cage.TickEngine
+{
+
+    public interface ITickEngineInputPort<TUnit>
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        Task PauseAsync(CancellationToken cancellationToken);
+
+        Task ResumeAsync(CancellationToken cancellationToken);
+
+        Task StartAsync(TUnit[] units, CancellationToken cancellationToken);
+
+        Task StopAsync(CancellationToken cancellationToken);
+
+        #endregion Methods
+
+    }
+
+}
