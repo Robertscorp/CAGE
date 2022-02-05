b849b119606258eed1f747121c0cdf035d9ab074
James Roberts <James@Robertscom.com>
2022-02-05 21:11:12 +1100

    Add the IUnit interface
    CAGE-7

---
+++ Cage.TickEngine/IUnit.cs
@@ -0,0 +1,27 @@
+﻿namespace Cage.TickEngine
+{
+
+    public interface IUnit<TPhase, TPlayer>
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        TPhase Phase { get; }
+
+        TPlayer Player { get; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        int GetRemainingTicksToNextPhase();
+
+        bool IsConscious();
+
+        bool IsDefeated();
+
+        #endregion Methods
+
+    }
+
+}
