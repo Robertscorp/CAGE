ca065feca7da32b60c2fdb1fcb63df7250f82061
James Roberts <James@Robertscom.com>
2022-02-05 21:09:44 +1100

    Add the WaitAsync WaitHandle extension method
    CAGE-7

---
+++ Cage.TickEngine/Extensions/WaitHandleExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace Cage.TickEngine.Extensions
+{
+    public static class WaitHandleExtensions
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        public static Task<bool> WaitAsync(this WaitHandle waitHandle, int? timeoutInMilliseconds = null)
+        {
+            if (Equals(waitHandle, null))
+                throw new ArgumentNullException(nameof(waitHandle));
+
+            var _TaskCompletionSource = new TaskCompletionSource<bool>();
+            var _RegisteredWaitHandle = ThreadPool.RegisterWaitForSingleObject(
+                waitHandle,
+                callBack: (state, timedOut) => _ = _TaskCompletionSource.TrySetResult(!timedOut),
+                state: null,
+                millisecondsTimeOutInterval: timeoutInMilliseconds ?? -1,
+                executeOnlyOnce: true);
+
+            return _TaskCompletionSource.Task.ContinueWith(task =>
+            {
+                _ = _RegisteredWaitHandle.Unregister(waitObject: null);
+                try
+                {
+                    return task.Result;
+                }
+                catch
+                {
+                    return false;
+                }
+            });
+        }
+
+        #endregion Methods
+
+    }
+
+}
