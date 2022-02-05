5556144b0ab5c0dc090831a030bc6595c10efe36
James Roberts <James@Robertscom.com>
2022-02-05 20:34:41 +1100

    Add the Tick Engine OutputPort
    CAGE-7

---
+++ Cage.TickEngine/ITickEngineOutputPort.cs
@@ -0,0 +1,35 @@
+﻿namespace Cage.TickEngine
+{
+
+    public interface ITickEngineOutputPort<TPlayer, TUnit>
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        Task EngineAlreadyRunningAsync(CancellationToken cancellationToken);
+
+        Task EnginePausedAsync(CancellationToken cancellationToken);
+
+        Task EngineResumedAsync(CancellationToken cancellationToken);
+
+        Task EngineStartedAsync(CancellationToken cancellationToken);
+
+        Task EngineStartFailureAsync(string reason, CancellationToken cancellationToken);
+
+        Task EngineStoppedAsync(CancellationToken cancellationToken);
+
+        Task PlayerDefeatedAsync(TPlayer player, CancellationToken cancellationToken);
+
+        Task TickAsync(CancellationToken cancellationToken);
+
+        Task UnitDefeatedAsync(TUnit unit, CancellationToken cancellationToken);
+
+        Task UnitStartPhaseAsync(TUnit unit, CancellationToken cancellationToken);
+
+        Task UnitTurnAsync(TUnit unit, CancellationToken cancellationToken);
+
+        #endregion Methods
+
+    }
+
+}
