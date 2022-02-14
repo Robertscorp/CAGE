181f31361264db8590868b45c66a4d96a851e178
James Roberts <James@Robertscom.com>
2022-02-14 22:02:10 +1100

    Add the ResourceCapacityChange Strategy
    CAGE-17

---
+++ Cage.CombatEngine.Tests/Unit/ResourcePools/ResourceCapacityChangeStrategyTests.cs
@@ -0,0 +1,98 @@
+﻿using Cage.CombatEngine.ResourcePools;
+using FluentAssertions;
+using Xunit;
+
+namespace Cage.CombatEngine.Tests.Unit.ResourcePools
+{
+
+    public class ResourceCapacityChangeStrategyTests
+    {
+
+        #region - - - - - - ChangeResourceByCapacity Tests - - - - - -
+
+        [Theory]
+        [InlineData(100, 100, 25, 25)]
+        [InlineData(100, 100, 150, 150)]
+        [InlineData(100, 100, -10, -10)]
+        [InlineData(100, 50, 25, -25)]
+        [InlineData(100, 50, 150, 100)]
+        [InlineData(100, 50, -25, -75)]
+        public void ChangeResourceByCapacity_VariousCapacityChanges_RemainingResourceFollowsCapacityChange(
+            int oldCapacity, int oldRemainingResource, int newCapacity, int expectedRemainingResource)
+            => ResourceCapacityChangeStrategies
+                .ChangeResourceByCapacity(oldCapacity, oldRemainingResource, newCapacity)
+                .Should().Be(expectedRemainingResource);
+
+        #endregion ChangeResourceByCapacity Tests
+
+        #region - - - - - - DecreaseResourceOnly Tests - - - - - -
+
+        [Theory]
+        [InlineData(100, 100, 25, 25)]
+        [InlineData(100, 100, 150, 100)]
+        [InlineData(100, 100, -10, -10)]
+        [InlineData(100, 50, 25, -25)]
+        [InlineData(100, 50, 150, 50)]
+        [InlineData(100, 50, -25, -75)]
+        public void DecreaseResourceOnly_VariousCapacityChanges_RemainingResourceFollowsCapacityChange(
+            int oldCapacity, int oldRemainingResource, int newCapacity, int expectedRemainingResource)
+            => ResourceCapacityChangeStrategies
+                .DecreaseResourceOnly(oldCapacity, oldRemainingResource, newCapacity)
+                .Should().Be(expectedRemainingResource);
+
+        #endregion DecreaseResourceOnly Tests
+
+        #region - - - - - - IncreaseResourceByCapacity Tests - - - - - -
+
+        [Theory]
+        [InlineData(100, 100, 25, 25)]
+        [InlineData(100, 100, 150, 150)]
+        [InlineData(100, 100, -10, -10)]
+        [InlineData(100, 50, 25, 25)]
+        [InlineData(100, 50, 150, 100)]
+        [InlineData(100, 50, -25, -25)]
+        public void IncreaseResourceByCapacity_VariousCapacityChanges_RemainingResourceFollowsCapacityChange(
+            int oldCapacity, int oldRemainingResource, int newCapacity, int expectedRemainingResource)
+            => ResourceCapacityChangeStrategies
+                .IncreaseResourceByCapacity(oldCapacity, oldRemainingResource, newCapacity)
+                .Should().Be(expectedRemainingResource);
+
+        #endregion IncreaseResourceByCapacity Tests
+
+        #region - - - - - - MaintainResourcePercentage Tests - - - - - -
+
+        [Theory]
+        [InlineData(100, 100, 25, 25)]
+        [InlineData(100, 100, 150, 150)]
+        [InlineData(100, 100, -10, -10)]
+        [InlineData(100, 75, 150, 112.5)]
+        [InlineData(100, 75, 50, 37.5)]
+        [InlineData(100, 75, -25, -18.75)]
+        public void MaintainResourcePercentage_VariousCapacityChanges_RemainingResourceFollowsCapacityChange(
+            int oldCapacity, int oldRemainingResource, int newCapacity, double expectedRemainingResource)
+            => ResourceCapacityChangeStrategies
+                .MaintainResourcePercentage(oldCapacity, oldRemainingResource, newCapacity)
+                .Should().Be((decimal)expectedRemainingResource);
+
+        #endregion MaintainResourcePercentage Tests
+
+        #region - - - - - - RetainRemainingResource Tests - - - - - -
+
+        [Theory]
+        [InlineData(100, 100, 25, 25)]
+        [InlineData(100, 100, 150, 100)]
+        [InlineData(100, 100, -10, -10)]
+        [InlineData(100, 75, 150, 75)]
+        [InlineData(100, 75, 50, 50)]
+        [InlineData(100, 75, -25, -25)]
+        public void RetainRemainingResource_VariousCapacityChanges_RemainingResourceFollowsCapacityChange(
+            int oldCapacity, int oldRemainingResource, int newCapacity, int expectedRemainingResource)
+            => ResourceCapacityChangeStrategies
+                .RetainRemainingResource(oldCapacity, oldRemainingResource, newCapacity)
+                .Should().Be(expectedRemainingResource);
+
+        #endregion RetainRemainingResource Tests
+
+    }
+
+}
---
+++ Cage.CombatEngine/ResourcePools/ResourceCapacityChangeStrategy.cs
@@ -0,0 +1,46 @@
+﻿namespace Cage.CombatEngine.ResourcePools
+{
+
+    public delegate decimal ResourceCapacityChangeStrategy(int oldCapacity, int oldRemainingResource, int newCapacity);
+
+    public static class ResourceCapacityChangeStrategies
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        public static readonly ResourceCapacityChangeStrategy ChangeResourceByCapacity = new(ChangeByCapacity);
+        public static readonly ResourceCapacityChangeStrategy DecreaseResourceOnly = new(DecreaseOnly);
+        public static readonly ResourceCapacityChangeStrategy IncreaseResourceByCapacity = new(IncreaseByCapacity);
+        public static readonly ResourceCapacityChangeStrategy MaintainResourcePercentage = new(MaintainPercentage);
+        public static readonly ResourceCapacityChangeStrategy RetainRemainingResource = new(RetainRemaining);
+
+        #endregion Fields
+
+        #region - - - - - - Methods - - - - - -
+
+        private static decimal ChangeByCapacity(int oldCapacity, int oldRemainingResource, int newCapacity)
+            => oldRemainingResource - oldCapacity + newCapacity;
+
+        private static decimal DecreaseOnly(int oldCapacity, int oldRemainingResource, int newCapacity)
+            => newCapacity >= oldCapacity
+                ? oldRemainingResource
+                : ChangeByCapacity(oldCapacity, oldRemainingResource, newCapacity);
+
+        private static decimal IncreaseByCapacity(int oldCapacity, int oldRemainingResource, int newCapacity)
+            => oldCapacity >= newCapacity
+                ? Math.Min(newCapacity, oldRemainingResource)
+                : ChangeByCapacity(oldCapacity, oldRemainingResource, newCapacity);
+
+        private static decimal MaintainPercentage(int oldCapacity, int oldRemainingResource, int newCapacity)
+            => (decimal)newCapacity * oldRemainingResource / oldCapacity;
+
+        private static decimal RetainRemaining(int oldCapacity, int oldRemainingResource, int newCapacity)
+            => Math.Min(oldRemainingResource, newCapacity);
+
+        #endregion Methods
+
+    }
+
+
+
+}
