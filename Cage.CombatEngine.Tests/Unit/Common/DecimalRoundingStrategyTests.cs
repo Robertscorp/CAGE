42f7a6b6cdf28f1d11f7c026edb6960a205a0728
James Roberts <James@Robertscom.com>
2022-02-14 20:39:10 +1100

    Add DecimalRoundingStrategy
    CAGE-17

---
+++ Cage.CombatEngine.Tests/Unit/Common/DecimalRoundingStrategyTests.cs
@@ -0,0 +1,85 @@
+﻿using Cage.CombatEngine.Common;
+using FluentAssertions;
+using Xunit;
+
+namespace Cage.CombatEngine.Tests.Unit.Common
+{
+
+    public class DecimalRoundingStrategyTests
+    {
+
+        #region - - - - - - AlwaysRoundDown Tests - - - - - -
+
+        [Theory]
+        [InlineData(0.0001D, 0D)]
+        [InlineData(0.4999D, 0D)]
+        [InlineData(0.5000D, 0D)]
+        [InlineData(0.5001D, 0D)]
+        [InlineData(0.9999D, 0D)]
+        [InlineData(-0.0001D, -0D)]
+        [InlineData(-0.4999D, -0D)]
+        [InlineData(-0.5000D, -0D)]
+        [InlineData(-0.5001D, -0D)]
+        [InlineData(-0.9999D, -0D)]
+        public void AlwaysRoundDown_VariousNumbers_AlwaysRoundsDownToNearestInteger(double @double, double expected)
+            => DecimalRoundingStrategies.AlwaysRoundDown((decimal)@double).Should().Be((decimal)expected);
+
+        #endregion AlwaysRoundDown Tests
+
+        #region - - - - - - AlwaysRoundUp Tests - - - - - -
+
+        [Theory]
+        [InlineData(0.0001D, 1D)]
+        [InlineData(0.4999D, 1D)]
+        [InlineData(0.5000D, 1D)]
+        [InlineData(0.5001D, 1D)]
+        [InlineData(0.9999D, 1D)]
+        [InlineData(-0.0001D, -1D)]
+        [InlineData(-0.4999D, -1D)]
+        [InlineData(-0.5000D, -1D)]
+        [InlineData(-0.5001D, -1D)]
+        [InlineData(-0.9999D, -1D)]
+        public void AlwaysRoundUp_VariousNumbers_AlwaysRoundsUpToNearestInteger(double @double, double expected)
+            => DecimalRoundingStrategies.AlwaysRoundUp((decimal)@double).Should().Be((decimal)expected);
+
+        #endregion AlwaysRoundUp Tests
+
+        #region - - - - - - HalfRoundDown Tests - - - - - -
+
+        [Theory]
+        [InlineData(0.0001D, 0D)]
+        [InlineData(0.4999D, 0D)]
+        [InlineData(0.5000D, 0D)]
+        [InlineData(0.5001D, 1D)]
+        [InlineData(0.9999D, 1D)]
+        [InlineData(-0.0001D, 0D)]
+        [InlineData(-0.4999D, 0D)]
+        [InlineData(-0.5000D, 0D)]
+        [InlineData(-0.5001D, -1D)]
+        [InlineData(-0.9999D, -1D)]
+        public void HalfRoundDown_VariousNumbers_HalfRoundsDownToNearestInteger(double @double, double expected)
+            => DecimalRoundingStrategies.HalfRoundDown((decimal)@double).Should().Be((decimal)expected);
+
+        #endregion HalfRoundDown Tests
+
+        #region - - - - - - HalfRoundUp Tests - - - - - -
+
+        [Theory]
+        [InlineData(0.0001D, 0D)]
+        [InlineData(0.4999D, 0D)]
+        [InlineData(0.5000D, 1D)]
+        [InlineData(0.5001D, 1D)]
+        [InlineData(0.9999D, 1D)]
+        [InlineData(-0.0001D, 0D)]
+        [InlineData(-0.4999D, 0D)]
+        [InlineData(-0.5000D, -1D)]
+        [InlineData(-0.5001D, -1D)]
+        [InlineData(-0.9999D, -1D)]
+        public void HalfRoundUp_VariousNumbers_HalfRoundsUpToNearestInteger(double @double, double expected)
+            => DecimalRoundingStrategies.HalfRoundUp((decimal)@double).Should().Be((decimal)expected);
+
+        #endregion HalfRoundUp Tests
+
+    }
+
+}
---
+++ Cage.CombatEngine/Common/DecimalRoundingStrategy.cs
@@ -0,0 +1,20 @@
+﻿namespace Cage.CombatEngine.Common
+{
+
+    public delegate decimal DecimalRoundingStrategy(decimal d);
+
+    public static class DecimalRoundingStrategies
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        public static readonly DecimalRoundingStrategy AlwaysRoundDown = new(d => Math.Truncate(d));
+        public static readonly DecimalRoundingStrategy AlwaysRoundUp = new(d => Math.Ceiling(Math.Abs(d)) * Math.Sign(d));
+        public static readonly DecimalRoundingStrategy HalfRoundDown = new(d => Math.Round(d));
+        public static readonly DecimalRoundingStrategy HalfRoundUp = new(d => Math.Round(d, MidpointRounding.AwayFromZero));
+
+        #endregion Fields
+
+    }
+
+}
