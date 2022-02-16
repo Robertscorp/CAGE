217db6856b8f2cfe8464408c0a514cdabe09c4e1
James Roberts <James@Robertscom.com>
2022-02-16 22:36:02 +1100

    Add the ResourcePool Input and Output ports
    CAGE-17

---
+++ Cage.CombatEngine/IResourcePoolInputPort.cs
@@ -0,0 +1,94 @@
+﻿using Cage.CombatEngine.Common;
+using Cage.CombatEngine.ResourcePools;
+
+namespace Cage.CombatEngine
+{
+
+    public interface IResourcePoolInputPort
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        ResourceID ResourceID { get; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        Task ChangeBaseCapacityAsync(ChangeBaseCapacityRequest request, CancellationToken cancellationToken);
+
+        Task ChangeCapacityModifierAsync(ChangeCapacityModifierRequest request, CancellationToken cancellationToken);
+
+        Task ConsumeResourceAsync(ConsumeResourceRequest request, CancellationToken cancellationToken);
+
+        Task RestoreResourceAsync(RestoreResourceRequest request, CancellationToken cancellationToken);
+
+        #endregion Methods
+
+    }
+
+    public struct ChangeBaseCapacityRequest
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public decimal BaseCapacityChange { get; set; }
+
+        public ResourceCapacityChangeStrategy CapacityChangeStrategy { get; set; }
+
+        public DecimalRoundingStrategy CapcityRoundingStrategy { get; set; }
+
+        public DecimalRoundingStrategy RemainingResourceRoundingStrategy { get; set; }
+
+        #endregion Properties
+
+    }
+
+    public struct ChangeCapacityModifierRequest
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public decimal CapacityModifierChange { get; set; }
+
+        public ResourceCapacityChangeStrategy CapacityChangeStrategy { get; set; }
+
+        public DecimalRoundingStrategy CapacityRoundingStrategy { get; set; }
+
+        public DecimalRoundingStrategy RemainingResourceRoundingStrategy { get; set; }
+
+        #endregion Properties
+
+    }
+
+    public struct ConsumeResourceRequest
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public decimal AmountToConsume { get; set; }
+
+        public bool ShouldCriticallyConsumeResource { get; set; }
+
+        public bool ShouldIgnoreConsumeModifier { get; set; }
+
+        #endregion Properties
+
+    }
+
+    public struct RestoreResourceRequest
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public decimal AmountToRestore { get; set; }
+
+        public bool ShouldIgnoreRestoreModifier { get; set; }
+
+        #endregion Properties
+
+    }
+
+    public class ResourceID { }
+
+}
---
+++ Cage.CombatEngine/IResourcePoolOutputPort.cs
@@ -0,0 +1,64 @@
+﻿namespace Cage.CombatEngine
+{
+
+    public interface IResourcePoolOutputPort
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        Task CapacityChangedAsync(CapacityChangedResponse response, CancellationToken cancellationToken);
+
+        Task ResourceConsumedAsync(ResourceConsumedResponse response, CancellationToken cancellationToken);
+
+        Task ResourceRestoredAsync(ResourceRestoredResponse response, CancellationToken cancellationToken);
+
+        #endregion Methods
+
+    }
+
+    public struct CapacityChangedResponse
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public decimal MaxCapacity { get; set; }
+
+        public decimal RemainingResource { get; set; }
+
+        public ResourceID ResourceID { get; set; }
+
+        #endregion Properties
+
+    }
+
+    public struct ResourceConsumedResponse
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public decimal RemainingResource { get; set; }
+
+        public decimal ResourceConsumed { get; set; }
+
+        public ResourceID ResourceID { get; set; }
+
+        #endregion Properties
+
+    }
+
+    public struct ResourceRestoredResponse
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public decimal RemainingResource { get; set; }
+
+        public ResourceID ResourceID { get; set; }
+
+        public decimal ResourceRestored { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
